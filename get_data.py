15d2fece4a617dc63e0f2d9cdd79de4cc7d3931c
Dhruv Thakur <dhruvt93@gmail.com>
2018-11-05 20:17:52 +0530

    added config yml file for fetching data

--- get_data.py
+++ get_data.py
@@ -4,36 +4,43 @@ import pprint
 import pickle
 from pathlib import Path
 import argparse
+import yaml
 
 parser = argparse.ArgumentParser(description='Fetch album art urls')
 parser.add_argument('genre', type=str,)
-parser.add_argument('artist_name', type=str,)
+# parser.add_argument('artist_name', type=str,)
+parser.add_argument('--dry-run', type=bool,)
 
 args = parser.parse_args()
 
+ARTISTS_CONFIG_FILE = 'artists.yml'
+
+with open('artists.yml') as f:
+    training_artists = yaml.load(f)
+
 GENRE = args.genre
 
-ARTIST_NAME = args.artist_name
+# ARTIST_NAME = args.artist_name
+try:
+    ARTISTS_TO_GET = training_artists['genres'][GENRE]
+except Exception as e:
+    print(str(e))
 
 SEEN_ARTISTS_FILENAME = 'seen_artists'
 
 seen_artists_file = Path(SEEN_ARTISTS_FILENAME)
 
+if seen_artists_file.is_file():
+    infile = open(seen_artists_file,'rb')
+    artists_pool = pickle.load(infile)
+    infile.close()
+else:
+    artists_pool = {}
+
 with open('token.txt') as f:
     TOKEN = f.read().strip()
 
-if seen_artists_file.is_file():
-    infile = open(seen_artists_file,'rb')
-    seen_artists = pickle.load(infile)
-    infile.close()
-else:
-    seen_artists = {}
-
-url = "https://api.spotify.com/v1/search"
-
-# ARTIST_NAME = "metallica"
-
-querystring = {"q":ARTIST_NAME,"type":"artist"}
+search_url = "https://api.spotify.com/v1/search"
 
 headers = {
     'Accept': "application/json",
@@ -42,46 +49,57 @@ headers = {
     'cache-control': "no-cache"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+for ARTIST_NAME in ARTISTS_TO_GET:
+    print(ARTIST_NAME)
 
-resp = json.loads(response.text)
-# pprint.pprint(resp)
-artist_id = resp["artists"]["items"][0]["id"]
+    search_query_string = {"q":ARTIST_NAME,"type":"artist"}
 
-# ------------
+    response = requests.request("GET", search_url, headers=headers, params=search_query_string)
 
-url = f'https://api.spotify.com/v1/artists/{artist_id}/related-artists'
+    resp = json.loads(response.text)
 
-response = requests.request("GET", url, headers=headers)
+    artist_id = resp["artists"]["items"][0]["id"]
 
-resp = json.loads(response.text)
+    similar_url = f'https://api.spotify.com/v1/artists/{artist_id}/related-artists'
 
-artist_ids = []
-artist_names = []
+    similar_response = requests.request("GET", similar_url, headers=headers)
 
-for artist in resp["artists"]:
-    if not ARTIST_NAME.lower() in seen_artists:
-        seen_artists[ARTIST_NAME.lower()] = artist["id"]
-        artist_ids.append(artist_id)
-        artist_names.append(ARTIST_NAME.lower())
-    # print(artist["name"], artist["id"])
-    if not artist["name"].lower() in seen_artists:
-        # seen_artists.append(artist["name"])
-        artist_ids.append(artist["id"])
-        artist_names.append(artist["name"].lower())
-        seen_artists[artist["name"].lower()] = artist["id"]
-    else:
-        print(f'Already seen {artist["name"].lower()}')
+    similar_resp = json.loads(similar_response.text)
+
+    for artist in similar_resp["artists"]:
+        if not ARTIST_NAME.lower() in artists_pool:
+            artists_pool[ARTIST_NAME.lower()] = artist["id"]
+
+        if not artist["name"].lower() in artists_pool:
+            artists_pool[artist["name"].lower()] = artist["id"]
+        else:
+            print(f'Already seen {artist["name"].lower()}')
+
+# pprint.pprint(artists_pool)
+
+# for artist in similar_resp["artists"]:
+#     if not ARTIST_NAME.lower() in seen_artists:
+#         seen_artists[ARTIST_NAME.lower()] = artist["id"]
+#         artist_ids.append(artist_id)
+#         artist_names.append(ARTIST_NAME.lower())
+#     # print(artist["name"], artist["id"])
+#     if not artist["name"].lower() in seen_artists:
+#         # seen_artists.append(artist["name"])
+#         artist_ids.append(artist["id"])
+#         artist_names.append(artist["name"].lower())
+#         seen_artists[artist["name"].lower()] = artist["id"]
+#     else:
+#         print(f'Already seen {artist["name"].lower()}')
 
 albums = {}
 
-for artist_id, artist_name in zip(artist_ids, artist_names):
-    print(artist_name)
+for artist_name, artist_id in artists_pool.items():
+    print(f'Fetching {artist_name}\'s albums')
     url = f'https://api.spotify.com/v1/artists/{artist_id}/albums'
 
-    albums_querystring = {"limit":"30"}
+    albums_query_string = {"limit":"30"}
 
-    response = requests.request("GET", url, headers=headers, params=albums_querystring)
+    response = requests.request("GET", url, headers=headers, params=albums_query_string)
 
     resp = json.loads(response.text)
 
@@ -98,7 +116,7 @@ for artist_id, artist_name in zip(artist_ids, artist_names):
     albums[artist_id] = artist_albums
 
 outfile = open(SEEN_ARTISTS_FILENAME,'wb')
-pickle.dump(seen_artists,outfile)
+pickle.dump(artists_pool,outfile)
 outfile.close()
 
 # pprint.pprint(albums)
